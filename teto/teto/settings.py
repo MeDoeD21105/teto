4b5bac671309f6ee65f3b2b23a5d6c39ced8d4b5
MeDoeD です <medoed21105@gmail.com>
2024-03-03 01:25:01 +0300

    start

--- teto/teto/settings.py
+++ teto/teto/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-d!ksl-@n%qyk#g+vwn8#%-4aivf3@lp*#rg0+*23&(6x8o%v$!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "tetoa.apps.TetoaConfig"
 ]
 
 MIDDLEWARE = [
@@ -103,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
@@ -116,6 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = []
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- teto/tetoa/models.py
+++ teto/tetoa/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length = 255)
+    content = models.TextField()
+    slug = models.SlugField(max_length = 255, unique = True, db_index = True )
+    photo = models.ImageField(upload_to="photos/", defaut = None)
+    price = models.DecimalField( max_digits = 7, decimal_places = 2)
+    quantity = models.IntegerField( default=0, validators=[MinValueValidator(0), MaxValueValidator(10000)])
+    
+                                 
