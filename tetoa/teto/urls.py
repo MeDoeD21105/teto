d0d58ec074ca7d9d45ba32fd7a89485c095e169c
MeDoeD です <medoed21105@gmail.com>
2024-03-09 02:58:32 +0300

    sdf

--- tetoa/teto/urls.py
+++ tetoa/teto/urls.py
@@ -5,5 +5,11 @@ from django.urls import path
 
 
 urlpatterns = [
-    path("", ProductClass.as_view(), name="Home")
-    ]
+    path("", ProductClass.as_view(), name="Home"),
+    path("add_prod/", ProdAddClass.as_view(), name= "add_prod"),
+    path("contact/", ContactClass.as_view(), name= "contact"),
+    path("post/<slug:post_slug>/", ShowPost.as_view(), name='post'),
+    #path("login/", LoginUserr.as_view(), name="login"),
+    #path("register/", RegisterUserr.as_view(), name="register"),
+    #path("users/", UsersClass.as_view(), name="userrs")
+]
--- tetoa/teto/views.py
+++ tetoa/teto/views.py
@@ -45,3 +45,15 @@ class ShowPost(DataMixin, DetailView):
         context["title"] = context["posts"].title
         return dict(list(context.items()) + list(c_def.items()))
     
+    
+class ContactClass(DataMixin,ListView):
+    model = Product
+    template_name = "teto/contact.html"
+
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context()
+        context["title"] = "Contact"
+        return dict(list(context.items()) + list(c_def.items()))
+    
